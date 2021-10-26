0bb01b0d4c58e67d294ecd672479387704caea16
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-10-26 11:29:55 +0800

    Added division funtion.

--- Basic-calculator-2.0.py
+++ Basic-calculator-2.0.py
@@ -1,34 +1,40 @@
-operation = int(input("\n Select one operation. \n Type 1 if addition, 2 if subtraction, 3 if multiplication, and 4 if division. \n Enter the number here: "))
+operation = int(input("\nSelect one operation. \nType 1 if addition, 2 if subtraction, 3 if multiplication, and 4 if division. \nEnter the number here: "))
 
 if operation == 1:
-    print(" \n Let's proceed to addition!")
+    print("\nLet's proceed to addition!")
 
-    num1 = float(input("\n Enter a number: "))
+    num1 = float(input("\nEnter a number: "))
     num2 = float(input("Enter another number: "))
 
     sum = num1+num2
-    print(f"{sum} is the sum of {num1} and {num2}.")
+    print(f"{sum} is the sum of {num1} and {num2}. \n ")
 
 if operation == 2:
-    print(" \n Let's proceed to subtraction!")
+    print("\nLet's proceed to subtraction!")
 
-    num1 = float(input("\n Enter the minuend: "))
+    num1 = float(input("\nEnter the minuend: "))
     num2 = float(input("Enter the subtrahend: "))
 
     difference = num1-num2
-    print(f"{difference} is the difference of {num1} and {num2}.")
+    print(f"{difference} is the difference of {num1} and {num2}. \n ")
 
 if operation == 3:
-    print(" \n Let's proceed to multiplication!")
-    
-    num1 = float(input("Enter the multiplicand: "))
+    print("\nLet's proceed to multiplication!")
+
+    num1 = float(input("\nEnter the multiplicand: "))
     num2 = float(input("Enter the multiplier: "))
     
     product = num1*num2
-    print(f"{product} is the product of {num1} and {num2}.")
+    print(f"{product} is the product of {num1} and {num2}. \n ")
 
 if operation == 4:
-     print(" \n Let's proceed to division!")
+     print("\nLet's proceed to division!")
+
+     num1 = float(input("\nEnter the dividend: "))
+     num2 = float(input("Enter the divisor: "))
+     
+     quotient = num1/num2
+     print(f"{quotient} is the quotient of {num1} and {num2}. \n ")
 
 if operation > 4:
     print(" \n Sorry, invalid number.")
