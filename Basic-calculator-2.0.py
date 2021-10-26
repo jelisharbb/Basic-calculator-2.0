984f536d35eb68554b0e138c0f085f401bdb1340
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-10-26 11:56:57 +0800

    Added exponentiation and remainder.

--- Basic-calculator-2.0.py
+++ Basic-calculator-2.0.py
@@ -1,4 +1,4 @@
-operation = int(input("\nSelect one operation. \nType 1 if addition, 2 if subtraction, 3 if multiplication, and 4 if division. \nEnter the number here: "))
+operation = int(input("\nSelect one operation. \nType 1 if addition, 2 if subtraction, 3 if multiplication, 4 if division, 5 if exponentiation, and 6 if remainder. \nEnter the number here: "))
 
 if operation == 1:
     print("\nLet's proceed to addition!")
@@ -36,5 +36,26 @@ if operation == 4:
      quotient = num1/num2
      print(f"{quotient} is the quotient of {num1} and {num2}. \n ")
 
-if operation > 4:
-    print(" \n Sorry, invalid number.")
+if operation == 5:
+     print("\nLet's proceed to exponentiation!")
+
+     num1 = float(input("\nEnter the number: "))
+     num2 = float(input("Enter the power or exponent: "))
+     
+     answer = num1**num2
+     print(f"{answer} is the answer when {num1} is raised to {num2}. \n ")
+
+if operation == 6:
+     print("\nLet's proceed to remainder!")
+
+     num1 = float(input("\nEnter the dividend: "))
+     num2 = float(input("Enter the divisor: "))
+     
+     remainder = (num1%num2)
+     print(f"{remainder} is the remainder when you divide {num1} and {num2}. \n ")
+
+if operation > 6:
+    print(" \n Sorry, invalid number. Please choose between 1-6.")
+
+elif operation == 0:
+    print(" \n Sorry, invalid number. Please choose between 1-6.")
